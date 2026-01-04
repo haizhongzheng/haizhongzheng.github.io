c946c0f1a6dc06254614564e8cf8fd2d8dea39b7
haizhongzheng <64877460+haizhongzheng@users.noreply.github.com>
2026-01-04 14:03:21 +0000

    Deploying to gh-pages from @ haizhongzheng/haizhongzheng.github.io@163bc5b8f7c2c0ace2a74c0649ab2fb0ffdb424b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "Teaching experience and courses taught.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
